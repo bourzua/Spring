3d5bc10a528eb943207b1a497fd3d54bb06245a0
jua <bourzua@naver.com>
2022-10-05 07:09:53 +0900

    관심사 분리

--- 김영한_Spring기본/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 김영한_Spring기본/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,14 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
 
     // 추상화에도 의존하고, 구체화에도 의존한다. => DIP 위반
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 추상화에만 의존
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- 김영한_Spring기본/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 김영한_Spring기본/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,20 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+//     할인 정책을 변경하려면 클라이언트인 'OrderServiceImpl' 코드를 고쳐야 한다.
+//    추상(인터페이스) 뿐만 아니라 구체(구현) 클래스에도 의존
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
