2d3cc942f8b5564184313d141c191da3f943d856
최주아 사원 DWP서비스팀 (zuaseyo@lgcns.com <zuaseyo@CNS.LGCNS.COM>
2022-10-26 18:41:28 +0900

    AppConfig

--- 김영한_Spring기본/core/src/main/java/hello/core/AppConfig.java
+++ 김영한_Spring기본/core/src/main/java/hello/core/AppConfig.java
@@ -15,6 +15,6 @@ public class AppConfig {
     public OrderService orderService() {
         return new OrderServiceImpl();
     }
-
-
 }
+
+
--- 김영한_Spring기본/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 김영한_Spring기본/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,22 +1,23 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
 
-//     할인 정책을 변경하려면 클라이언트인 'OrderServiceImpl' 코드를 고쳐야 한다.
-//    추상(인터페이스) 뿐만 아니라 구체(구현) 클래스에도 의존
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
+    //     할인 정책을 변경하려면 클라이언트인 'OrderServiceImpl' 코드를 고쳐야 한다.
+    //    추상(인터페이스) 뿐만 아니라 구체(구현) 클래스에도 의존
+    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
